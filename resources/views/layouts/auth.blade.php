18f945e4bfcfa26891b809c6750155cc95c00cb6
Muhammad Khairin <88011174+mkhairin@users.noreply.github.com>
2025-09-29 12:05:15 +0800

    Login blade view

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -26,41 +26,11 @@
 </head>
 
 <body>
-    @if (auth()->check())
-        <div class="container-scroller">
 
-            <livewire:partials.navbar />
 
-            <div class="container-fluid page-body-wrapper">
-
-                <livewire:partials.sidebar />
-
-                <div class="main-panel">
-                    <div class="content-wrapper">
-                        {{ $slot }}
-                    </div>
-
-                    <!-- partial:partials/_footer.html -->
-                    <footer class="footer">
-                        <div class="d-sm-flex justify-content-center justify-content-sm-between">
-                            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2024
-                                <a href="https://www.bootstrapdash.com/" target="_blank">Bootstrapdash</a>. All rights
-                                reserved.</span>
-                            <span class="float-none float-sm-end d-block mt-1 mt-sm-0 text-center">Hand-crafted & made
-                                with
-                                <i class="mdi mdi-heart text-danger"></i></span>
-                        </div>
-                    </footer>
-                    <!-- partial -->
-
-                </div>
-            </div>
-        </div>
-    @else
-        <main>
-            {{ $slot }}
-        </main>
-    @endif
+    <main>
+        {{ $slot1 }}
+    </main>
 
 
     <!-- plugins:js -->
